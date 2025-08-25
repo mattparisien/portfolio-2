c2835e1c0292a4db893ad1a42cc0b300a97c2cda
Matt Parisien <63109316+mattparisien@users.noreply.github.com>
2025-08-25 16:37:39 -0400

    S

--- src/app/api/cloudinary-images/route.ts
+++ src/app/api/cloudinary-images/route.ts
@@ -8,11 +8,6 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-console.log('Cloudinary configuration:', {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET ? '***' : 'not set',
-});
 
 export async function GET() {
   try {
